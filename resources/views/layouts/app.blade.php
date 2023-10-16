82e6d06e82607a0a2d8c92d2d98b4c1c04504096
wunnaSCM <scm.wunna@gmail.com>
2023-10-16 15:41:43 +0630

    laravel mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite('resourrces/js/app.js')
+    {{-- @vite(['resources/css/app.css', 'resourrces/js/app.js']) --}}
 </head>
 
 <body class="font-sans antialiased">
