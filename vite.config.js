cf425d328b3211b243039dc0426eef1ba10ebb61
wunnaSCM <scm.wunna@gmail.com>
2023-10-17 08:38:24 +0630

    change vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    build: {
+        /** If you set esmExternals to true, this plugins assumes that 
+              all external dependencies are ES modules */
+
+        commonjsOptions: {
+            esmExternals: true,
+        },
+    },
 });
