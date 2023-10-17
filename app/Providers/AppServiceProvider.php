b5514080b9f349a86b10fcf0c70f29a60877d420
wunnaSCM <scm.wunna@gmail.com>
2023-10-17 14:04:38 +0630

    update url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,8 +26,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if ($this->app->environment('production')) {
+            $url::forceScheme('https');
+        }
     }
 }
