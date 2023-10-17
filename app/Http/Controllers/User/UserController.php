38cc6aec1b1124110994ffd8367e15245ec4f309
wunnaSCM <scm.wunna@gmail.com>
2023-10-17 16:31:05 +0630

    change store

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -108,7 +108,7 @@ class UserController extends Controller
     {
         $imageName = time() . '.' . $request->profile->extension();
         $request->profile->move(storage_path('app/public/user_image/'), $imageName);
-        $request->profle->storeAs('app/public/user_image/', $imageName); // storage/app/images/file.png
+        $request->profle->storeAs('user_image/', $imageName); // storage/app/images/file.png
         return $imageName;
     }
 
